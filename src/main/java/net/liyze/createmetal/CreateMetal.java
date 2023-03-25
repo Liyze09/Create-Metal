6304431fb45c62e85f9bd9bd470885fbee23f5be
Liyze09 <a52990419@outlook.com>
2023-03-25 13:36:19 +0800

    Codestyle

--- src/main/java/net/liyze/createmetal/CreateMetal.java
+++ src/main/java/net/liyze/createmetal/CreateMetal.java
@@ -1,5 +1,5 @@
-package net.liyze.createmetal;
 //First Mod
+package net.liyze.createmetal;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
@@ -25,79 +25,105 @@ import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
 import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 
 import java.util.Arrays;
+
 public class CreateMetal implements ModInitializer {
     //Item Classes
     //Tin
     public static final Item TIN_INGOT;
     static {
-        TIN_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));}
+        TIN_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    }
     public static final Item RAW_TIN;
     static {
-        RAW_TIN = new Item(new FabricItemSettings().group(ItemGroup.MISC));}
+        RAW_TIN = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    }
     public static final Item TIN_NUGGET;
     static {
-        TIN_NUGGET = new Item(new FabricItemSettings().group(ItemGroup.MISC));}
+        TIN_NUGGET = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    }
     //Bronze
     public static final Item BRONZE_INGOT;
     static {
-        BRONZE_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));}
+        BRONZE_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    }
     //Steel
     public  static final Item STEEL_INGOT;
     static {
-        STEEL_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC).fireproof());}
+        STEEL_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC).fireproof());
+    }
     //Block Classes
     //Steel
     public static final Block STEEL_BLOCK;
     static {
-        STEEL_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(6.0F, 10.0F).sounds(BlockSoundGroup.METAL));}
+        STEEL_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(6.0F, 10.0F).sounds(BlockSoundGroup.METAL));
+    }
     public static final BlockItem STEEL_BLOCK_ITEM;
     static {
-        STEEL_BLOCK_ITEM = new BlockItem(STEEL_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS).fireproof());}
+        STEEL_BLOCK_ITEM = new BlockItem(STEEL_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS).fireproof());
+    }
     //Bronze
     public static final Block BRONZE_BLOCK;
     static {
-        BRONZE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(4.0F, 6.0F).sounds(BlockSoundGroup.METAL));}
+        BRONZE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(4.0F, 6.0F).sounds(BlockSoundGroup.METAL));
+    }
     public static final BlockItem BRONZE_BLOCK_ITEM;
     static {
-        BRONZE_BLOCK_ITEM = new BlockItem(BRONZE_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));}
+        BRONZE_BLOCK_ITEM = new BlockItem(BRONZE_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
+    }
     //Tin
     public static final Block TIN_ORE_BLOCK;
     static {
-        TIN_ORE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE));}
+        TIN_ORE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0F, 3.0F).sounds(BlockSoundGroup.STONE));
+    }
     public static final BlockItem TIN_ORE_ITEM;
     static {
-        TIN_ORE_ITEM = new BlockItem(TIN_ORE_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));}
+        TIN_ORE_ITEM = new BlockItem(TIN_ORE_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
+    }
     public static final Block TIN_DEEP_ORE;
     static {
-        TIN_DEEP_ORE = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(4.5F, 3.0F).sounds(BlockSoundGroup.DEEPSLATE));}
+        TIN_DEEP_ORE = new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(4.5F, 3.0F).sounds(BlockSoundGroup.DEEPSLATE));
+    }
     public static final BlockItem TIN_DEEP_ORE_ITEM;
     static {
-        TIN_DEEP_ORE_ITEM = new BlockItem(TIN_DEEP_ORE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));}
+        TIN_DEEP_ORE_ITEM = new BlockItem(TIN_DEEP_ORE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
+    }
     public static final Block TIN_BLOCK;
     static {
-        TIN_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.METAL));}
+        TIN_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.METAL));
+    }
     public static final BlockItem TIN_BLOCK_ITEM;
     static {
-        TIN_BLOCK_ITEM = new BlockItem(TIN_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));}
+        TIN_BLOCK_ITEM = new BlockItem(TIN_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
+    }
     public static final Block RAW_TIN_BLOCK;
     static {
-        RAW_TIN_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.METAL));}
+        RAW_TIN_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.METAL));
+    }
     public static final BlockItem RAW_TIN_BLOCK_ITEM;
     static {
-        RAW_TIN_BLOCK_ITEM = new BlockItem(RAW_TIN_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));}
+        RAW_TIN_BLOCK_ITEM = new BlockItem(RAW_TIN_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS));
+    }
     //Worldgen
+    //Tin Ore
     public static final ConfiguredFeature<?, ?> OVERWORLD_TIN_ORE_CONFIGURED_FEATURE;
     static  {
-        OVERWORLD_TIN_ORE_CONFIGURED_FEATURE = new ConfiguredFeature<>(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, TIN_ORE_BLOCK.getDefaultState(), 6));}
+        OVERWORLD_TIN_ORE_CONFIGURED_FEATURE = new ConfiguredFeature<>(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.STONE_ORE_REPLACEABLES, TIN_ORE_BLOCK.getDefaultState(), 6));
+    }
     public static final PlacedFeature OVERWORLD_TIN_ORE_PLACED_FEATURE;
     static {
-        OVERWORLD_TIN_ORE_PLACED_FEATURE = new PlacedFeature(RegistryEntry.of(OVERWORLD_TIN_ORE_CONFIGURED_FEATURE), Arrays.asList(CountPlacementModifier.of(20), SquarePlacementModifier.of(), HeightRangePlacementModifier.uniform(YOffset.fixed(0), YOffset.fixed(128))));}
+        OVERWORLD_TIN_ORE_PLACED_FEATURE = new PlacedFeature(RegistryEntry.of(OVERWORLD_TIN_ORE_CONFIGURED_FEATURE),
+                Arrays.asList(CountPlacementModifier.of(20), SquarePlacementModifier.of(), HeightRangePlacementModifier.uniform(YOffset.fixed(0), YOffset.fixed(128))));
+    }
+    //Deepslate Tin Ore
     public static final ConfiguredFeature<?, ?> OVERWORLD_DEEP_TIN_ORE_CONFIGURED_FEATURE;
     static  {
-        OVERWORLD_DEEP_TIN_ORE_CONFIGURED_FEATURE = new ConfiguredFeature<>(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, TIN_DEEP_ORE.getDefaultState(), 6));}
+        OVERWORLD_DEEP_TIN_ORE_CONFIGURED_FEATURE = new ConfiguredFeature<>(Feature.ORE, new OreFeatureConfig(OreConfiguredFeatures.DEEPSLATE_ORE_REPLACEABLES, TIN_DEEP_ORE.getDefaultState(), 6));
+    }
     public static final PlacedFeature OVERWORLD_DEEP_TIN_ORE_PLACED_FEATURE;
     static {
-        OVERWORLD_DEEP_TIN_ORE_PLACED_FEATURE = new PlacedFeature(RegistryEntry.of(OVERWORLD_DEEP_TIN_ORE_CONFIGURED_FEATURE), Arrays.asList(CountPlacementModifier.of(20), SquarePlacementModifier.of(), HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(0))));}
+        OVERWORLD_DEEP_TIN_ORE_PLACED_FEATURE = new PlacedFeature(RegistryEntry.of(OVERWORLD_DEEP_TIN_ORE_CONFIGURED_FEATURE),
+                Arrays.asList(CountPlacementModifier.of(20), SquarePlacementModifier.of(), HeightRangePlacementModifier.uniform(YOffset.getBottom(), YOffset.fixed(0))));
+    }
     @Override
     public void onInitialize(){
         //Tin
@@ -151,12 +177,14 @@ public class CreateMetal implements ModInitializer {
                 "createmetal", "tin_ore"), OVERWORLD_TIN_ORE_CONFIGURED_FEATURE);
         Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(
                 "createmetal", "tin_ore"), OVERWORLD_TIN_ORE_PLACED_FEATURE);
-        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES,
+                RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(
                 "createmetal", "tin_ore")));
         //Tin Deepslate Ores
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(
                 "createmetal", "deepslate_tin_ore"), OVERWORLD_DEEP_TIN_ORE_CONFIGURED_FEATURE);
         Registry.register(BuiltinRegistries.PLACED_FEATURE, new Identifier(
                 "createmetal", "deepslate_tin_ore"), OVERWORLD_DEEP_TIN_ORE_PLACED_FEATURE);
-        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES,
+                RegistryKey.of(Registry.PLACED_FEATURE_KEY, new Identifier(
                 "createmetal", "deepslate_tin_ore")));}}
